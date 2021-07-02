c57c2013bfb54dd17743820ded459f7a0131fd41
emanoel <vianna.emanoel@gmail.com>
2021-07-01 21:30:51 -0300

    initial commit

---
+++ src/main/java/com/credit/on/demand/credit/simulation/Application.java
@@ -0,0 +1,13 @@
+package com.credit.on.demand.credit.simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/credit/on/demand/credit/simulation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.on.demand.credit.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
